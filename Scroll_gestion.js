c8cb0b787c5fc87abdc226fa8b1984c9f7d258d2
siar2112 <87784451+siar2112@users.noreply.github.com>
2022-11-20 00:21:33 -0500

    mc

--- Scroll_gestion.js
+++ Scroll_gestion.js
@@ -1,29 +1,35 @@
+
+const esprit = document.querySelector('.esprit');
+
+
 const observer2 = new IntersectionObserver(entries => {
   entries.forEach(entry => {
 
-    const latest_Merch_container_1 = entry.target.querySelector('.Latest_Merch_container:nth-child(1)');
-    const latest_Merch_container_2 = entry.target.querySelector('.Latest_Merch_container:nth-child(2)');
-    const latest_Merch_container_3 = entry.target.querySelector('.Latest_Merch_container:nth-child(3)');
-    const latest_Merch_container_4 = entry.target.querySelector('.Latest_Merch_container:nth-child(4)');
-    const latest_Merch_container_5 = entry.target.querySelector('.Latest_Merch_container:nth-child(5)');
+    const Gestion_box_container_1 = entry.target.querySelector('.Gestion_box_list li:nth-child(1)');
+    const Gestion_box_container_2 = entry.target.querySelector('.Gestion_box_list li:nth-child(2)');
+    const Gestion_box_container_3 = entry.target.querySelector('.Gestion_box_list li:nth-child(3)');
+    const Gestion_box_container_4 = entry.target.querySelector('.Gestion_box_list li:nth-child(4)');
+    const Gestion_box_container_5 = entry.target.querySelector('.Gestion_box_list li:nth-child(5)');
 
     if (entry.isIntersecting) {
       console.log("yes");
-      latest_Merch_container_1.classList.add('Latest_Merch_container_animate');
-      latest_Merch_container_2.classList.add('Latest_Merch_container_animate');
-      latest_Merch_container_3.classList.add('Latest_Merch_container_animate');
-      latest_Merch_container_4.classList.add('Latest_Merch_container_animate');
-      latest_Merch_container_5.classList.add('Latest_Merch_container_animate');
+      Gestion_box_container_1.classList.add('box');
+      Gestion_box_container_2.classList.add('box');
+      Gestion_box_container_3.classList.add('box');
+      Gestion_box_container_4.classList.add('box');
+      Gestion_box_container_5.classList.add('box');
+      esprit.classList.add('fade');
       //console.log("yes");
 	  return; // if we added the class, exit the function
     }
     //console.log("no more");
-    latest_Merch_container_1.classList.remove('Latest_Merch_container_animate');
-    latest_Merch_container_2.classList.remove('Latest_Merch_container_animate');
-    latest_Merch_container_3.classList.remove('Latest_Merch_container_animate');
-    latest_Merch_container_4.classList.remove('Latest_Merch_container_animate');
-    latest_Merch_container_5.classList.remove('Latest_Merch_container_animate');
+    Gestion_box_container_1.classList.remove('box');
+    Gestion_box_container_2.classList.remove('box');
+    Gestion_box_container_3.classList.remove('box');
+    Gestion_box_container_4.classList.remove('box');
+    Gestion_box_container_5.classList.remove('box');
+    esprit.classList.remove('fade');
   });
 });
 
-observer2.observe(document.querySelector('.Latest_Merch_list'));
+observer2.observe(document.querySelector('.Gestion_box_list'));
